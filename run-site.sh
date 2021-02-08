6386df8d375d033220a9cb90097e0b3235fc6d1c
Daniel West <daniel@silverback.is>
2021-02-08 17:06:51 +0000

    Update for latest ansible and master token ID. (Should be configurable)

--- run-site.sh
+++ run-site.sh
@@ -6,12 +6,12 @@ ssh-add ~/.ssh/id_rsa
 # ~/Library/Python/2.7/bin/ansible-playbook
 # for user uuid run `openstack user show <username>`
 ansible-playbook site.yaml -e "reource_prefix=sbwa \
-key_name=mac-pro \
+key_name=macbook-pro-default \
 flavor_master=UKF2-std-8x8 \
-flavor_worker=UKF2-std-2x2 \
+flavor_worker=UKF2-std-4x4 \
 image=af97568c-e513-424d-823e-d95caee94554 \
 external_network_name=vlan2803 \
-worker_node_number=1 \
+worker_node_number=2 \
 auth_url=$OS_AUTH_URL \
 user_id=$OS_USER_UUID \
 password=$OS_PASSWORD \
